b61939af40630e6122df9bba8b6bdd2b215d2661
datkanber <burak2kanber@gmail.com>
2024-03-25 23:05:52 +0300

    posapp v3.2C bill

---
+++ client/src/components/cart/CreateBill.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { Form, Modal, Input, Select, Button, Card } from 'antd';
+
+const countryCodes = [
+    { code: '+90', country: 'Turkey' },
+    { code: '+1', country: 'United States' },
+    { code: '+44', country: 'United Kingdom' },
+    // Add more country codes as needed
+];
+
+const CreateBill = ({ isModalOpen, setIsModalOpen }) => {
+    const [form] = Form.useForm(); // Form.useForm hook'undan form nesnesi oluşturuldu
+
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+
+    return (
+        <Modal
+            title="Fatura Oluştur"
+            open={isModalOpen}
+            footer={false}
+            onCancel={() => setIsModalOpen(false)}
+        >
+            <Form layout={'vertical'} form={form} onFinish={onFinish}> {/* form nesnesini Form bileşenine ilettik */}
+                <Form.Item
+                    label="Müşteri Adı"
+                    name={'customerName'}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Müşteri Adı Alanı Doldurulmalıdır.',
+                        },
+                    ]}
+                >
+                    <Input placeholder="Müşteri Adı Yazınız" />
+                </Form.Item>
+                <Form.Item
+                    label="Telefon Numarası"
+                    name={'phoneNumber'}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Telefon Numarası Girmelisiniz.',
+                        },
+                    ]}
+                >
+                    <div style={{ display: 'flex', width: '100%' }}>
+                        <Select
+                            style={{ width: '110px', marginRight: '8px' }}
+                            placeholder="Ülke Kodu"
+                            onChange={(value) => form.setFieldsValue({ phoneNumber: { ...form.getFieldValue('phoneNumber'), code: value } })}
+                        >
+                            {countryCodes.map((item) => (
+                                <Select.Option key={item.code} value={item.code}>
+                                    {item.code}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                        <Input
+                            style={{ flexGrow: 1 }}
+                            placeholder="Telefon Numaranızı Giriniz"
+                            maxLength={15}
+                            onChange={(e) => form.setFieldsValue({ phoneNumber: { ...form.getFieldValue('phoneNumber'), number: e.target.value } })}
+                        />
+                    </div>
+                </Form.Item>
+                <Form.Item
+                    label="Ödeme Yöntemi"
+                    name={'billMethod'}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Ödeme Yöntemi Seçmelisiniz.',
+                        },
+                    ]}
+                >
+                    <Select placeholder="Ödeme Yöntemi Seçiniz">
+                        <Select.Option value="Nakit">Nakit</Select.Option>
+                        <Select.Option value="Kredi Kartı">Kredi Kartı</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Card>
+                    <div
+                        className="flex justify-between">
+                        <span>Ara Toplam</span>
+                        <span>549.00TL</span>
+                    </div>
+                    <div
+                        className="flex justify-between my-2">
+                        <span>KDV Toplam %8</span>
+                        <span className="text-red-600">+43.93TL</span>
+                    </div>
+                    <div
+                        className="flex justify-between">
+                        <b>Toplam</b>
+                        <b>549.00TL</b>
+                    </div>
+                    <div className="flex justify-end">
+                        <Button
+                            className="mt-4"
+                            type="primary"
+                            size="large"
+                            onClick={() => setIsModalOpen(true)}
+                            htmlType="submit"
+                        >Sipariş Oluştur</Button>
+                    </div>
+                </Card>
+            </Form>
+        </Modal>
+    );
+};
+
+export default CreateBill
--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,8 +1,12 @@
-import { Table, Card } from "antd";
+import { Table, Card, Button } from "antd";
 import Header from "../components/header/Header";
+import { useState } from "react";
+import CreateBill from "../components/cart/CreateBill";
+
 
 
 const CartPage = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const dataSource = [
         {
             key: '1',
@@ -36,23 +40,50 @@ const CartPage = () => {
         },
     ];
 
+    console.log(isModalOpen);
+
     return (
         <>
             <Header />
-            <div className="px-6">
-                <Table dataSource={dataSource} columns={columns} bordered />
-                <div className="cart-total flex justify-end">
-                    <Card className="w-72">
-                        <div className="flex justify-between">
+            <div
+                className="px-6">
+                <Table
+                    dataSource={dataSource}
+                    columns={columns}
+                    bordered pagination={false} />
+                <div
+                    className="cart-total flex justify-end mt-4">
+                    <Card
+                        className="w-72">
+                        <div
+                            className="flex justify-between">
                             <span>Ara Toplam</span>
                             <span>549.00TL</span>
-                
                         </div>
+                        <div
+                            className="flex justify-between my-2">
+                            <span>KDV Toplam %8</span>
+                            <span className="text-red-600">+43.93TL</span>
+                        </div>
+                        <div
+                            className="flex justify-between">
+                            <b>Toplam</b>
+                            <b>549.00TL</b>
+                        </div>
+                        <Button
+                            className="w-full mt-4"
+                            type="primary"
+                            size="large"
+                            onClick={() => setIsModalOpen(true)}
+                        >Sipariş Oluştur</Button>
                     </Card>
                 </div>
             </div>
+            <CreateBill
+                isModalOpen={isModalOpen}
+                setIsModalOpen={setIsModalOpen} />
         </>
-    )
-}
+    );
+};
 
-export default CartPage
+export default CartPage;
